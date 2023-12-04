bc3808e7f2f51d4ad47577e7634648a5dac39306
Raghunandan Rajput <hariomrajput123h@gmail.com>
2023-12-04 17:29:56 +0530

    Password-Generator

--- password-Generator/src/main.jsx
+++ password-Generator/src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import PwdGeneter from './PwdGeneter.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <PwdGeneter/>
+    <App />
   </React.StrictMode>,
 )
---
+++ vite-project/src/PwdGeneter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PwdGeneter = () => {
+  return (
+    <div>PwdGeneter</div>
+  )
+}
+
+export default PwdGeneter
